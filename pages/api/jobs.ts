cb38d4056b0c22633394a20b1f61705924035daa
daniel-black <drblack651@gmail.com>
2022-11-17 17:44:21 -0700

    Add /api/jobs route

---
+++ pages/api/jobs.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { query } = req;
+
+  // const searchParams = new URLSearchParams(query);
+  
+  const request = await fetch(
+    `https://data.usajobs.gov/api/search?Keyword=park`, 
+    {
+      headers: {
+        'Authorization-Key': process.env.JOBS_API_KEY || ''
+      }
+    }
+  );
+
+  const requestData = await request.json();
+
+  res.status(200).json({ requestData });
+}
